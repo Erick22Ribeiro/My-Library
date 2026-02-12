781bbc88dee354c77ef7121098dc9983fb97c976
Erick Ribeiro <erick2ribeirogg@gmail.com>
2026-02-12 14:53:50 -0300

    Menu lateral

--- core/services/googlebooks.py
+++ core/services/googlebooks.py
@@ -25,6 +25,7 @@ def procurar_livros_g(query):
             'ano': info.get('publishedDate'),
             'capa': info.get('imageLinks', {}).get('thumbnail'),
             'categoria': info.get('categories', ['Categoria desconhecida'])[0],
+            'num_paginas': info.get('pageCount'),
         })
 
     return livros
