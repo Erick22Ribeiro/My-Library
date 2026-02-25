32637d168dcfbc13714c6c0c98cf331eba3992b6
Erick Ribeiro <erick2ribeirogg@gmail.com>
2026-02-25 16:35:28 -0300

    Opções de salvar(Para ler / comprado / lido)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Livro(models.Model):
+
+    TIPO_CHOICES = [
+        ('quero', 'Quero Ler'),
+        ('lido', 'Lido'),
+        ('comprado', 'Comprado'),
+    ]
+
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    ano = models.CharField(max_length=10, blank=True)
+    paginas = models.IntegerField(default=0)
+    categoria = models.CharField(max_length=100, blank=True)
+    capa = models.URLField(blank=True)
+
+    tipo = models.CharField(
+        max_length=10,
+        choices = TIPO_CHOICES
+    )
+
+    def __str__(self):
+
+        return self.titulo
+
+
--- core/views.py
+++ core/views.py
@@ -1,19 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.services.openlibrary import procurar_livros
 from core.services.googlebooks import procurar_livros_g
+from core.models import Livro
 
 # Create your views here.
 def home(request):
 
-    query = request.GET.get("q")
+    livros = Livro.objects.all()
 
-    livros = []
-
-    if query:
-        #livros = procurar_livros(query)  #API Open Library
-        livros = procurar_livros_g(query)  #API Google Books
-
-    return render(request, "core/home.html", {"livros": livros, "query": query})
+    context = {
+        'livros': livros
+    }
+    
+    return render(request, "core/home.html", context)
 
 
 def buscar(request):
@@ -26,7 +25,26 @@ def buscar(request):
         #livros = procurar_livros(query)  #API Open Library
         livros = procurar_livros_g(query)  #API Google Books
 
+    if request.method == 'POST':
+
+        #Livro.objects.all().delete()
+
+        Livro.objects.create(
+            
+            titulo = request.POST['titulo'],
+            categoria = request.POST['categoria'],
+            autor = request.POST['autor'],
+            ano = request.POST['ano'],
+            paginas = request.POST['paginas'],
+            capa = request.POST['capa'],
+            tipo = request.POST['tipo'],
+
+        )
+
+        return redirect('buscar')
+
     return render(request, "core/buscar.html", {"livros": livros, "query": query})
+
 
 def livros(request):
 
